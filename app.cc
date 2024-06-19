3ed3cae12a5cd8c61baba502866094e7d5906dc4
Nikos Dragazis <dragazisn@gmail.com>
2024-06-20 00:32:35 +0300

    Add simple HTTP server
    Create a simple HTTP server.
    The code is influenced by the Seastar httpd app.
    
    Refs #3.
    
    Signed-off-by: Nikos Dragazis <dragazisn@gmail.com>

--- app.cc
+++ app.cc
@@ -1,15 +1,46 @@
 #include <seastar/core/app-template.hh>
 #include <seastar/util/log.hh>
+#include <seastar/http/httpd.hh>
+#include <seastar/http/handlers.hh>
+#include <seastar/http/function_handlers.hh>
+#include <seastar/core/thread.hh>
+
+#include "stop_signal.hh"
 
 using namespace seastar;
 
 logger applog(__FILE__);
 
+void set_routes(httpd::routes& r) {
+    httpd::function_handler* h1 = new httpd::function_handler([](httpd::const_req req) {
+        return "hello";
+    });
+    r.add(httpd::operation_type::GET, httpd::url("/"), h1);
+}
+
+future<int> run_http_server() {
+    return async([] {
+        seastar_apps_lib::stop_signal stop_signal;
+        net::inet_address addr("127.0.0.1");
+        uint16_t port = 9999;
+        httpd::http_server_control server;
+        std::cout << "starting HTTP server" << std::endl;
+        server.start().get();
+        std::cout << "setting routes for HTTP server" << std::endl;
+        server.set_routes(set_routes).get();
+        std::cout << "listening on HTTP server" << std::endl;
+        server.listen(port).get();
+        std::cout << "HTTP server listening on port " << port << " ...\n";
+        std::cout << "stopping HTTP server" << std::endl;
+        stop_signal.wait().get();
+        server.stop().get();
+        std::cout << "HTTP server stopped" << std::endl;
+        return 0;
+    });
+}
+
 int main(int argc, char** argv) {
     app_template app;
 
-    return app.run(argc, argv, [&app] {
-        applog.info("HELLO WORLD");
-        return make_ready_future<>();
-    });
+    return app.run(argc, argv, run_http_server);
 }
